2ce0b1a872c9db3d8ac7d528dd86c8e24295a862
asoto <asoto@mgage.com>
2017-11-28 11:31:38 -0800

    Updating sequelize objects to pull skills from database and exposing with express

--- server/core/api.js
+++ server/core/api.js
@@ -1,16 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('mysql://root:root@localhost:3306/mudb');
 
-
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
-
 const Class = sequelize.define('class', {
     id: {
         type: Sequelize.INTEGER,
@@ -23,10 +13,53 @@ const Class = sequelize.define('class', {
         timestamps: false,
         freezeTableName: true,
 
-});
+    });
 
-Class.findAll().then(users => {
-    console.log(users)
+const Skill = sequelize.define('skill', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING
+    },
+    description: {
+        type: Sequelize.STRING
+    },
+    damage: {
+        type: Sequelize.STRING
+    },
+    type: {
+        type: Sequelize.STRING
+    }
+}, {
+        timestamps: false,
+        freezeTableName: true,
+
+    });
+    
+const ClassAndSkills = sequelize.define('class_skill', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    class_id: {
+        type: Sequelize.INTEGER
+    },
+    skill_id: {
+        type: Sequelize.INTEGER
+    },
+}, {
+        timestamps: false,
+        freezeTableName: true,
+
+    });
+
+Class.belongsToMany(Skill, { as: 'skillsFromClass', through: ClassAndSkills, foreignKey: 'class_id' })
+Skill.belongsToMany(Class, { as: 'class', through: ClassAndSkills, foreignKey: 'skill_id' })
+
+Class.getSkillsFromClass().then(associatedTasks => {
+    // associatedTasks is an array of tasks
 })
 
 // var express = require('express');
