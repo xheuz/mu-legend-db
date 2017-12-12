5db95ac75d87201f5460d05d8b4d6f5d4a89ec3d
Victor Natschke <vnatschke@gmail.com>
2017-12-12 17:45:50 -0400

    added skills component and refactoring class-details component

--- client/src/classes/class-detail.component.jsx
+++ client/src/classes/class-detail.component.jsx
@@ -1,97 +1,19 @@
 import React, { Component } from 'react';
-import { Badge, Row, Col } from 'react-bootstrap';
+import Skills from '../skills/skill.component';
 import './class-detail.css';
-
-const url = 'http://ec2-52-27-199-57.us-west-2.compute.amazonaws.com:7475';
-const images_url = "http://mulegendb.info/server/images/skills";
-
-const groupByType = function(arr) {
-    let groups = {};
-
-    // arr.map( (e) => {
-    //     console.log("from function", e)
-    // });
-    for(var element in arr){
-        if (groups.hasOwnProperty(arr[element].type)) {
-            groups[arr[element].type].push(arr[element]);
-        } else {
-            groups[arr[element].type] = [];
-            groups[arr[element].type].push(arr[element]);
-        }
-    }
-
-    return groups;
-}
 
 class ClassDetails extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            skills: []
-        }
+        this.state = {}
     }
 
     componentDidMount() {
-        this.SkillList()
-    }
-
-    SkillList() {
-        fetch(url + '/classes/' + this.props.match.params.classid + '/skills')
-        .then( (results) => results.json())
-        .then(data => {
-            data = groupByType(data);
-            let skills = {};
-
-            for(var type in data) {
-                skills[type] = data[type].map((skill, indx) => {
-                    return(
-                        <Col xs={12} md={4} key={skill.id}>
-                        <div className="skill">
-                            <div className="skill-header">
-                                <img className="skill-image float-left" src={images_url + '/' + skill.id + '.png'} alt={"skill-" + skill.name} />
-                                <span className="skill-name">{skill.name}</span>
-                                <Badge bsClass={"badge skill-kind skill-" + skill.kind.toLowerCase()}>{skill.kind.toLowerCase()}</Badge>
-                            </div>
-                            <div className="skill-body">
-                                <p>{skill.description}</p>
-                                {/* <Badge>type: {skill.type}</Badge> &nbsp; */}
-                                {skill.damage !== null ? <Badge>damage: {skill.damage}% </Badge> : ""}
-                            </div>
-                        </div>
-                    </Col>
-                    )
-                });
-            }
-            this.setState({skills: skills});
-        });
     }
 
     render(){
-        let key = 0;
-
-        for(var property in this.state.skills) {
-            let columns = [];
-            let indx = 0;
-            for(var skill in this.state.skills[property]) {
-                if (indx % 3 === 0) {
-                    columns.push(
-                        <Row key={key}>{this.state.skills[property].slice(indx,indx+3)}</Row>
-                    );
-                    key++;
-                }
-                indx++;
-            }
-            this.state.skills[property] = columns;
-        };
         return(
-            <div>
-                <h2>Weapon</h2>
-                {this.state.skills['Weapon']}
-                <h2>Class</h2>
-                {this.state.skills['Class']}
-                <h2>Expert</h2>
-                {this.state.skills['Expert']}
-            </div>
+            <Skills {...this.props}/>
         )
     }
 }
--- client/src/common/header.component.jsx
+++ client/src/common/header.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem} from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import './header.css';
 
 class Navigation extends Component {
     render() {
@@ -9,13 +10,24 @@ class Navigation extends Component {
                     <Navbar.Brand>
                         <a href="/">Mu Legend Database</a>
                     </Navbar.Brand>
+                    <Navbar.Toggle />
                 </Navbar.Header>
-                <Nav>
-                    <NavItem eventKey={1} href="/">Home</NavItem>
-                    <NavItem eventKey={2} href="/classes">Classes</NavItem>
-                    <NavItem eventKey={3} href="/instances">Instances</NavItem>
-                    <NavItem eventKey={4} href="/items">Items</NavItem>
-                </Nav>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem eventKey={1} href="/">Home</NavItem>
+                        <NavItem eventKey={2} href="/classes">Classes</NavItem>
+                        <NavItem eventKey={3} href="/instances">Instances</NavItem>
+                        <NavItem eventKey={4} href="/items">Items</NavItem>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavDropdown eventKey={3} title="Login" id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}>Profile</MenuItem>
+                            <MenuItem eventKey={3.2}>Account</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.3}>Logout</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         );
     }    
